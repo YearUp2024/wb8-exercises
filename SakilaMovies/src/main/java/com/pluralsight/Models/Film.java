a6f5ce8479bb9f2643e11ae8559a261818f731e9
iftekar2 <iftekar.hossain123@gmail.com>
2024-12-04 14:52:17 -0500

    Adding and implementing Film class

---
+++ SakilaMovies/src/main/java/com/pluralsight/Models/Film.java
@@ -0,0 +1,62 @@
+package com.pluralsight.Models;
+
+public class Film {
+    private int filmId;
+    private String title;
+    private String description;
+    private String releaseYear;
+    private String length;
+
+    public Film(int filmId, String title, String description, String releaseYear, String length) {
+        this.filmId = filmId;
+        this.title = title;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.length = length;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return filmId + ": "+ title + " " + description + " " + releaseYear + " " + length;
+    }
+}
